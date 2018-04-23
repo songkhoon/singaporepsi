73256f919e9de5ef81a96d210e9cb765ba351371
Lim <t_khoon@hotmail.com>
2018-04-23 22:24:16 +0800

    Add Google Map

--- app/src/main/java/com/songkhoon/singaporepsi/MainActivity.java
+++ app/src/main/java/com/songkhoon/singaporepsi/MainActivity.java
@@ -3,11 +3,29 @@ package com.songkhoon.singaporepsi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupGoogleMap();
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    private void setupGoogleMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
 }
