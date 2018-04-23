2e60e9a976c4cbbb6283e26c2454561ee6e0dcea
Lim <t_khoon@hotmail.com>
2018-04-23 22:29:47 +0800

    Zoom to Singapore map

--- app/src/main/java/com/songkhoon/singaporepsi/MainActivity.java
+++ app/src/main/java/com/songkhoon/singaporepsi/MainActivity.java
@@ -3,11 +3,16 @@ package com.songkhoon.singaporepsi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final LatLng singapore = new LatLng(1.35735, 103.82);
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        this.googleMap = googleMap;
+        setupSingaporeMap();
     }
 
     private void setupGoogleMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
 
+    private void setupSingaporeMap() {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore, 10.5f));
     }
 
 }
